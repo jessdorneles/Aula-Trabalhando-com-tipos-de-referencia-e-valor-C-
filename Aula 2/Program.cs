a861380cd2cc49e769da3d627d6649dd165766cc
jessdorneles <jes.dorneles@gmail.com>
2022-04-21 13:57:19 -0300

    Aula 2

---
+++ Aula 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+class Aula2
+{
+    static void Adicionar20(int a)
+    {
+        a += 20;
+    }
+
+    static void Main()
+    {
+        int a = 5;
+        Adicionar20(a);
+        WriteLine($"O valor de a é {a}"!);
+        
+    
+    }
+}
